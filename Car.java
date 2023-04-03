f6fe6bfcab61e555cf047aa542f9bf1a9de09dc4
Johnny Ma <>
2023-04-03 04:20:30 +0000

    Initial commit

---
+++ Car.java
@@ -0,0 +1,10 @@
+public class Car {
+  private int count = 0;
+
+  public void sayCarCount() {
+    System.out.println(count);
+  }
+  public void sayHello(){
+    System.out.println("hi");
+  }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    // System.out.println("Hello world!");
+    Car car = new Car();
+    car.sayCarCount();
+    car.sayHello();
+  }
+}
